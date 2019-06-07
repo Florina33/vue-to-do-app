70aa91ac4929a3a26c0c39991eb15db8b03da95a
Svetlana <florina33@gmail.com>
2019-06-07 20:38:28 +0300

    create component AddTodo.vue and import it in main component App.vue

--- vue_crash_todolist/src/App.vue
+++ vue_crash_todolist/src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
-    <Header />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <Header/>
+    <AddTodo/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import Header from './components/layout/Header'
-import Todos from './components/Todos'
+import Header from "./components/layout/Header";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    Todos
+    Header,
+    Todos,
+    AddTodo
   },
   data() {
     return {
@@ -22,18 +26,18 @@ export default {
           title: "Todo One",
           completed: false
         },
-          {
+        {
           id: 2,
           title: "Todo Two",
           completed: true
         },
-          {
+        {
           id: 3,
           title: "Todo Three",
           completed: false
         }
       ]
-    }
+    };
   },
   methods: {
     deleteTodo(id) {
@@ -43,17 +47,31 @@ export default {
       this.todos = this.todos.filter(todo => todo.id !== id);
     }
   }
-}
+};
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #667;
+  }
 </style>
